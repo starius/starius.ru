dcc78b7a4ddc9a8b1f694866d00187f6090a1f48
Boris Nagaev <bnagaev@gmail.com>
2012-02-12 23:16:21 +0100

    init script: create functions for start and stop

--- wt-classes/examples/wtclasses-examples.sh
+++ wt-classes/examples/wtclasses-examples.sh
@@ -22,34 +22,32 @@ set -e
 mkdir -p `dirname $PIDFILE`
 chown www-data:www-data `dirname $PIDFILE`
 
+wtclasses_examples_start () {
+    start-stop-daemon --start --quiet --background --pidfile $PIDFILE \
+    --chuid www-data:www-data --exec $APP -- --docroot /usr/share/Wt/ \
+    -p $PIDFILE --http-port 50396 --http-address 127.0.0.1 || true
+}
+
+
+wtclasses_examples_stop () {
+    start-stop-daemon --stop --quiet --pidfile $PIDFILE --exec $APP || true
+}
+
 case "$1" in
     start)
         echo -n "Starting $DESC: "
-
-        start-stop-daemon --start --quiet --background --pidfile $PIDFILE \
-        --chuid www-data:www-data --exec $APP -- --docroot /usr/share/Wt/ \
-        -p $PIDFILE --http-port 50396 --http-address 127.0.0.1 || true
-
+        wtclasses_examples_start
         echo "$NAME."
         ;;
     stop)
         echo -n "Stopping $DESC: "
-
-        start-stop-daemon --stop --quiet --pidfile $PIDFILE --exec $APP || true
-
+        wtclasses_examples_stop
         echo "$NAME."
         ;;
     restart|force-reload|reload)
         echo -n "Restarting $DESC: "
-
-        start-stop-daemon --stop --quiet --pidfile $PIDFILE --exec $APP || true
-
-        sleep 1
-
-        start-stop-daemon --start --quiet --background --pidfile $PIDFILE \
-        --chuid www-data:www-data --exec $APP -- --docroot /usr/share/Wt/ \
-        -p $PIDFILE --http-port 50396 --http-address 127.0.0.1 || true
-
+        wtclasses_examples_start
+        wtclasses_examples_stop
         echo "$NAME."
         ;;
     status)
